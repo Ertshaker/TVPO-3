cb362aa385a63abcfe45138778e79514b5fd832c
Ertshaker <maxim.earthpower@gmail.com>
2025-10-09 16:17:25 +0700

    test2

--- tests/test_morgage.py
+++ tests/test_morgage.py
@@ -30,7 +30,6 @@ def test_overpayment(calculator):
     over = calculator.overpayment(1_000_000, 20_000, 5)
     assert over == 200_000  # 20к*60 = 1.2 млн → переплата 200к
 
-## ВСЕМ ПРИВЕТ
-##ddad
-
-##loolssвв
+'''
+3
+'''
